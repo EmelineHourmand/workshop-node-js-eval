2016d710d144762f579d0c3b95353d600f5fb93f
Emeline Hourmand <emeline.hourmand@gmail.com>
2017-12-12 17:10:19 +0100

    Exercice 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -2,6 +2,10 @@ exports.run = function(input) {
   console.log("Exercise #4");
 
   // YOUR CODE IS WELCOME HERE
+  function myFunction () {
+    var str = input
+    return str
+  }
 
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(myFunction, [1, 2, 3], ["One", "Two", "Three"]);
